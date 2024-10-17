c90a29414e19e9ba70647d46b4a7ba146fb6592f
Felipe Quesada <felipe.quesadam@gmail.com>
2024-10-17 01:43:34 -0600

    Pregunta 3 añadidos (sin dot)

---
+++ Test.py
@@ -0,0 +1,13 @@
+# This will display "Hello, World!" in the terminal
+print("Hello, World!")
+# This will display your system version in the terminal
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+print(sys.version)
+
+
+
+
+
